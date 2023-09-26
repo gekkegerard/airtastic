9586018198fc88315589c20654504520609f1d9d
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-09-26 16:09:16 +0200

    Homepagina aangemaakt

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:airtastic/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Homescreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
