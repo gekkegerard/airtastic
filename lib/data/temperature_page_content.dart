3d04b6088f513a06460857651d188c766d850992
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-10-10 12:17:47 +0200

    Begin aan de temperatuur pagina

---
+++ lib/data/temperature_page_content.dart
@@ -0,0 +1,7 @@
+const firstHeader = 'What is temperature?';
+const firstParagraph =
+    'Temperature is a physical quantity that expresses quantitatively the attribute of hotness or coldness.';
+const secondHeader = 'Effects of temperature on the body';
+const secondParagraph =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const thirdHeader = 'What is the ideal temperature?';
---
+++ lib/pages/Temperature.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:airtastic/nav_bar.dart';
+import 'package:airtastic/data/temperature_page_content.dart';
+
+class TemperaturePage extends StatefulWidget {
+  const TemperaturePage({super.key});
+
+  @override
+  State<TemperaturePage> createState() => _TemperaturePageState();
+}
+
+class _TemperaturePageState extends State<TemperaturePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: const nav_bar(),
+        appBar: AppBar(
+          title: const Text('Temperature'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                firstHeader,
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                firstParagraph,
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ],
+        ));
+  }
+}
