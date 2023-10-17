1fdabd1a66b903bf9ca28d4b6947d3946f1d1caa
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-10-17 12:58:05 +0200

    Content voor humidity en carbon dioxide gemaakt

---
+++ lib/data/carbon_dioxide_page_content.dart
@@ -0,0 +1,7 @@
+const navBarHeader = 'Carbon Dioxide';
+const firstHeader = 'What is carbon dioxide?';
+const firstParagraph = '';
+const secondHeader = 'Effects of carbon dioxide on the body';
+const secondParagraph = '';
+const thirdHeader = 'What is the ideal room carbon dioxide?';
+const thirdParagraph = '';
---
+++ lib/data/humidity_page_content.dart
@@ -0,0 +1,14 @@
+const navBarHeader = 'Humidity';
+const firstHeader = 'What is humidity?';
+const firstParagraph =
+    'Humidity is the concentration of water vapor present in the air. Water vapor, the gaseous state of water, is generally invisible to the human eye. Humidity indicates the likelihood for precipitation, dew, or fog to be present. Humidity depends on the temperature and pressure of the system of interest. The same amount of water vapor results in higher relative humidity in cool air than warm air. Humidity is measured in percentage.';
+const secondHeader = 'Effects of humidity on the body';
+const secondParagraph =
+    'High humidity can have an adverse effect on the human body. Because the air feels warmer than the official, recorded temperature, it can contribute to feelings of low energy and lethargy. In addition, hyperthermia,  or over-heating as a result of your body’s inability to effectively let out heat, can negatively impact your health in conditions of high humidity. Some health risks which result from overexposure to humidity (hyperthermia) include: dehydration, fatigue, muscle cramps, heat exhaustion, fainting, heat stroke.';
+const thirdHeader = 'What is the ideal room humidity?';
+const thirdParagraph =
+    'According to the Environmental Protection Agency, the best indoor relative humidity falls between 30% and 50%, and it should never exceed 60%. Other studies suggest 40% to 60% is a better range. Regardless, 60% seems to be the agreed-upon threshold for indoor humidity.';
+
+// https://en.wikipedia.org/wiki/Humidity
+// https://share.upmc.com/2014/06/effects-humidity-body/
+// https://www.sleepfoundation.org/bedroom-environment/humidity-and-sleep
