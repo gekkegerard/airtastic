5757a2003f8fe0c2e372c98fd22e6d8029c353cf
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-10-17 13:48:14 +0200

    Content pagina's compleet gemaakt

--- lib/data/carbon_dioxide_page_content.dart
+++ lib/data/carbon_dioxide_page_content.dart
@@ -1,7 +1,15 @@
 const navBarHeader = 'Carbon Dioxide';
 const firstHeader = 'What is carbon dioxide?';
-const firstParagraph = '';
+const firstParagraph =
+    'Carbon dioxide is a chemical compound with the chemical formula CO2. It is found in the gas state at room temperature. CO2 is measured in parts per million (ppm). CO2 is an important greenhouse gas that helps to trap heat in our atmosphere. Without it, our planet would be inhospitably cold. Human beings do exhale almost three billion tons of carbon dioxide annually, but the carbon we exhale is the same carbon that was “inhaled” from the atmosphere by the plants we consume.';
 const secondHeader = 'Effects of carbon dioxide on the body';
-const secondParagraph = '';
+const secondParagraph =
+    'American academics found that health problems can be caused by exposure to CO2 levels as low as 1,000 ppm. CO2 can cause health problems such as inflammation, reduced cognitive performance and kidney and bone problems.';
 const thirdHeader = 'What is the ideal room carbon dioxide?';
-const thirdParagraph = '';
+const thirdParagraph =
+    'To minimize the risk of airborne transmission of viruses, CO2 levels should be as low as possible in all indoor spaces. It is recommended to stay close to 400 ppm, (outdoor CO2 concentration), and below 800 ppm. If the threshold is exceeded, it is recommended to leave the room and to renew the air. ';
+
+// https://en.wikipedia.org/wiki/Carbon_dioxide
+// https://www.nrdc.org/stories/do-we-exhale-carbon
+// https://airqualitynews.com/health/co2-affects-human-health-at-lower-levels-than-previously-thought/
+// https://meersens.com/indoor-air-quality-the-impact-of-co2-on-health-and-wellbeing-at-work/?lang=en
---
+++ lib/data/ozone_page_content.dart
@@ -0,0 +1,13 @@
+const navBarHeader = 'Ozone';
+const firstHeader = 'What is ozone?';
+const firstParagraph =
+    "Ozone is a gas composed of three atoms of oxygen (O3). Ozone occurs both in the Earth's upper atmosphere and at ground level. Ozone can be good or bad, depending on where it is found. Called stratospheric ozone, good ozone occurs naturally in the upper atmosphere, where it forms a protective layer that shields us from the sun's harmful ultraviolet rays. This beneficial ozone has been partially destroyed by manmade chemicals, causing what is sometimes called a 'hole in the ozone.' Ozone at ground level (man-made ozone) is a harmful air pollutant, because of its effects on people and the environment, and it is the main ingredient in 'smog'.";
+const secondHeader = 'Effects of ozone on the body';
+const secondParagraph =
+    'Breathing ground-level ozone can trigger a variety of health problems including chest pain, coughing, throat irritation, and congestion. It can worsen bronchitis, emphysema, and asthma. Ozone also can reduce lung function and inflame the lining of the lungs. Its effect is especially during the summer months when temperatures are high.';
+const thirdHeader = 'What causes ground-level ozone?';
+const thirdParagraph =
+    'Ground level ozone, is not emitted directly into the air, but is created by chemical reactions between oxides of nitrogen (NOx) and volatile organic compounds (VOC). This happens when pollutants emitted by cars, power plants, industrial boilers, refineries, chemical plants, and other sources chemically react in the presence of sunlight.';
+
+// https://www.epa.gov/ozone-pollution-and-your-patients-health/what-ozone
+// https://www.epa.gov/ground-level-ozone-pollution/ground-level-ozone-basics
