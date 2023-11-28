c1bb7f551b5fff765e99a1c7065e9f068c99c401
GekkeGerard <gekkegerard1000@gmail.com>
2023-11-28 12:24:06 +0100

    kleine updates

--- lib/z_old_code/line_chart_widget.dart
+++ lib/z_old_code/line_chart_widget.dart
@@ -1,6 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:airtastic/data/random_chart_data.dart';
+import 'package:airtastic/z_old_code/random_chart_data.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
