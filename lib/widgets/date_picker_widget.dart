208319cdff3a70f83f68085dd260c4f02a43496f
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-12-12 10:26:09 +0100

    Datepicker toont nu alleen datums die ook in de DB zitten

--- lib/widgets/date_picker_widget.dart
+++ lib/widgets/date_picker_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class DatePickerWidget extends StatefulWidget {
   final Function(DateTime) onDateSelected;
+  final DateTime initialDate;
 
-  const DatePickerWidget({super.key, required this.onDateSelected});
+  const DatePickerWidget(
+      {super.key, required this.onDateSelected, required this.initialDate});
 
   @override
   // ignore: library_private_types_in_public_api
@@ -14,35 +18,98 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
   // Initialize the selected user date with the current date
   DateTime _selectedDate = DateTime.now();
 
-  // Assume these dates come from the server
-  List<String> datesFromServer = [
-    '2023-12-02',
-    '2023-11-30',
-    '2023-11-26',
-    '2023-11-18',
-    '2023-11-17',
-    '2023-11-16'
-  ];
-
   // Parse the dates and store them in a list
   List<DateTime> selectableDates = [];
 
   // Add a variable to store the last selected date
   DateTime? _lastSelectedDate;
 
+  // Loading indicator to show progress
+  bool _isLoading = true;
+
   @override
   void initState() {
+    print('initing date picker widget');
     super.initState();
-    selectableDates =
-        datesFromServer.map((date) => DateTime.parse(date)).toList();
+    _fetchDatesFromServer();
   }
 
+  // Fetch the data from the server response
+  Future<void> _fetchDatesFromServer() async {
+    try {
+      List<DateTime> dates = await getDatesFromServer();
+      setState(() {
+        selectableDates = dates;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print('Error fetching dates: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to fetch dates from the server')),
+      );
+    }
+  }
+
+  // Do a GET request for the available dates from the server
+  Future<List<DateTime>> getDatesFromServer() async {
+    List<DateTime> datesList = [];
+    try {
+      List<Map<String, dynamic>> datesFromServer;
+      var url =
+          'https://markus.glumm.sites.nhlstenden.com/opdracht11_app_get_unique_dates.php'; // GET = last day of measurements, POST = specific date
+
+      // Send a GET request to the server
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        datesFromServer =
+            List<Map<String, dynamic>>.from(jsonDecode(response.body));
+        if (datesFromServer.isEmpty) {
+          throw Exception('Server returned an empty unique dates list');
+        }
+      } else {
+        throw Exception('Failed to load the unique dates from the server');
+      }
+
+      // Decode the JSON response and cast it to a list of strings
+      datesFromServer =
+          List<Map<String, dynamic>>.from(jsonDecode(response.body));
+
+      print("Dates from server: $datesFromServer"); // DEBUG
+
+      // Parse the dates and store them in a list
+      setState(() {
+        datesList = datesFromServer
+            .map((dateMap) => DateTime.parse(dateMap['date']))
+            .toList();
+      });
+
+      print("Dates from server: $datesFromServer"); // DEBUG
+    } catch (e) {
+      // Handle the error by showing a message to the user
+      print('Error unique dates: $e');
+
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('Failed to fetch unique dates from the server')),
+      );
+    }
+
+    // Return the dates from the server
+    return datesList;
+  }
+
+  // Execute this function when the date picker is pressed
   Future<void> _selectDate(BuildContext context) async {
     final DateTime dateNow = DateTime.now();
     final DateTime? picked = await showDatePicker(
       helpText: "Show measurements of a date",
       context: context,
-      initialDate: selectableDates[0],
+      initialDate: widget.initialDate,
       firstDate: DateTime(2023, 11, 16),
       lastDate: dateNow,
       selectableDayPredicate: (DateTime date) {
@@ -82,13 +149,17 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () => _selectDate(context),
-      style: OutlinedButton.styleFrom(
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.red[600],
-      ),
-      child: const Text('Select Date'),
-    );
+    if (_isLoading) {
+      return const CircularProgressIndicator(); // or any loading indicator
+    } else {
+      return OutlinedButton(
+        onPressed: () => _selectDate(context),
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.red[600],
+        ),
+        child: const Text('Select Date'),
+      );
+    }
   }
 }
