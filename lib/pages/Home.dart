bfcf63bd7184ffd7dfec71ef360460cbfb224e3f
Markus0699 <105637596+Markus0699@users.noreply.github.com>
2023-12-19 16:24:09 +0100

    Homepage updated

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -3,9 +3,7 @@ import 'package:airtastic/widgets/nav_bar.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
-import 'package:airtastic/widgets/scatter_chart_widget.dart';
-//import 'package:airtastic/pages/settings.dart';
-//import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,75 +13,76 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool isActive = false; // Set this to false for a green circle
-  List<TemperatureData> temperatureDataList = [];
-  late Timer _timer;
-  var refreshTime = 30;
-  var loadingRefreshTime = 10;
-  bool isGraphLoaded = false;
+  bool espIsActive = false; // Set this to false for a green circle
+  String temperature = "?"; // Temperature in degrees Celsius
+  String humidity = "?"; // Humidity in percent
+  String ozone = "?"; // Ozone concentration in ppb
+  bool ozoneMeasurementValid = false;
+  DateTime lastDataPoint = DateTime(
+      2000); // Initialize this with a placeholder date '2000-01-01 00:00:00.000'
+  String uptimeESP = "?"; // Running time of the ESP in minutes
 
   @override
   void initState() {
     super.initState();
     // Try to get data initially
-    fetchData().then((success) {
-      if (success) {
-        // If initial fetch is successful, start periodic timer with refresh time of 30 seconds
-        _timer = Timer.periodic(Duration(seconds: refreshTime), (Timer timer) {
-          fetchData();
-        });
-      } else {
-        // If initial fetch fails, continue with periodic timer using loading refresh time of 10 seconds
-        _timer = Timer.periodic(Duration(seconds: loadingRefreshTime),
-            (Timer timer) {
-          fetchData();
-        });
-      }
-    });
+    fetchData(context).then((value) => setState(() {}));
   }
 
-  @override
-  void dispose() {
-    // Cancel the timer to prevent memory leaks
-    _timer.cancel();
-    super.dispose();
-  }
-
-  late double temperature = 20;
-  late double ozone = 20;
-  late double humidity = 20;
-  late int valid = 1;
-
-  Future<bool> fetchData() async {
+  Future<bool> fetchData(BuildContext context) async {
     try {
-      var url =
-          'https://markus.glumm.sites.nhlstenden.com/opdracht11_app_get_data.php'; // Get all the data from the last day of measuremtens
-      http.Response response = await http.get(Uri.parse(url));
+      const String serverUrl =
+          'https://markus.glumm.sites.nhlstenden.com/opdracht11_app_get_data.php';
+      http.Response? response;
+
+      // Create a body for the POST request, only request the last data point in the database
+      Map<String, String> body = {
+        'requestSize': "1",
+      };
+
+      // Encode the body to a string so it can be sent with the request
+      String encodedBody = body.keys
+          .map((key) => '$key=${Uri.encodeComponent(body[key]!)}')
+          .join('&');
+
+      // Send the POST request
+      response = await http.post(
+        Uri.parse(serverUrl),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: encodedBody,
+      );
+
+      // print("POST response from server:"); // DEBUG
+      // print(response.body); // DEBUG
+
       var data = jsonDecode(response.body) as List<dynamic>;
 
-      List<TemperatureData> tempList = [];
-
+      // Loop through the data and set the variables
       for (var entry in data) {
-        DateTime timestamp = DateTime.parse(entry['timestamp']);
-        temperature = double.parse(entry['temperature']);
-        ozone = double.parse(entry['ozone_concentration']);
-        humidity = double.parse(entry['humidity']);
-        valid = int.parse(entry['ozone_measurement_valid']);
-        tempList.add(TemperatureData(timestamp, temperature));
+        lastDataPoint = DateTime.parse(entry['timestamp']);
+        temperature = double.parse(entry['temperature']).toString();
+        ozone = double.parse(entry['ozone_concentration']).toString();
+        humidity = double.parse(entry['humidity']).toString();
+        ozoneMeasurementValid =
+            entry['ozone_measurement_valid'].toString().toLowerCase() == 'true';
+        uptimeESP = double.parse(entry['uptime']).toStringAsFixed(0);
       }
-
-      if (mounted) {
-        // Check if the widget is still in the widget tree before calling setState
-        setState(() {
-          temperatureDataList = tempList;
-          isGraphLoaded = true; // Set to true when the graph is loaded
-        });
-      }
-
       // Return true to indicate a successful fetch
       return true;
     } catch (e) {
-      print('Failed to fetch data: $e');
+      // Something went wrong
+      // print(e.toString()); // DEBUG
+
+      // Check if the widget is still active
+      if (mounted) {
+        // Let the user know that the fetch failed
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('The webserver failed to respond.'),
+          ),
+        );
+      }
+
       // Return false to indicate a failed fetch
       return false;
     }
@@ -91,14 +90,16 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    DateTime datum = temperatureDataList.isNotEmpty
-        ? temperatureDataList.first.timestamp
-        : DateTime.now();
+    const double paddingBetweenDataValues = 4;
 
-    if (valid != 0) {
-      isActive = true;
+    // Find out if the device has been active in the last 2 minutes
+    DateTime currentTime = DateTime.now();
+    Duration difference = currentTime.difference(lastDataPoint);
+    // Last measurement was less than 2 minutes ago
+    if (difference.inMinutes < 2) {
+      espIsActive = true;
     } else {
-      isActive = false;
+      espIsActive = false;
     }
 
     return Scaffold(
@@ -108,108 +109,147 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Active:',
-                    style: TextStyle(
-                      fontSize: 18,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: Text(
+                      "Welcome!",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: isActive ? Colors.green : Colors.red,
+                ],
+              ),
+              const Text(
+                "This is the home page where you can see the latest data from your Airtastic device. Take a look around the app if you like. You can refresh the data by pressing the refresh button in the bottom right corner.",
+                textAlign: TextAlign.center,
+                style: TextStyle(),
+              ),
+              const SizedBox(
+                  height:
+                      30), // Spacer between introduction and active indicator
+              const SizedBox(
+                  height: 30), // Spacer between active indicator and data
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Last measurement",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Currently active: ',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: espIsActive ? Colors.green : Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Date: ',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  lastDataPoint != DateTime(2000)
+                      ? Text(DateFormat('dd-MM-yyyy').format(lastDataPoint))
+                      : const Text("?")
+                ],
+              ),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Time: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  lastDataPoint != DateTime(2000)
+                      ? Text(DateFormat('HH:mm:ss').format(lastDataPoint))
+                      : const Text("?")
+                ],
+              ),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text(
+                  'Temperature: ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Ontime:'),
+                Text("$temperature °C"),
+              ]),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text(
+                  'Humidity: ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                Text("data markus")
-              ],
-            ),
-            const SizedBox(height: 200),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Date:'),
+                Text("$humidity %"),
+              ]),
+              const SizedBox(height: paddingBetweenDataValues),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text(
+                  'Ozone: ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                Text("${datum.day}-${datum.month}-${datum.year}")
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Temperature:'),
-                ),
-                Text("$temperature")
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Humidity:'),
-                ),
-                Text("$humidity")
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Ozone:'),
-                ),
-                Text("$ozone")
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text("these are the last measurd values"),
-                ),
-              ],
-            ),
-          ],
+                Text("$ozone PPB"),
+              ]),
+              const SizedBox(height: paddingBetweenDataValues), //
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    espIsActive
+                        ? 'Airtastic is currently online for $uptimeESP minutes.'
+                        : 'Airtastic was last online for $uptimeESP minutes.',
+                    style: const TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Refresh the data when the button is pressed
+          fetchData(context).then((value) => setState(() {}));
+        },
+        backgroundColor: Colors.red[600],
+        child: const Icon(Icons.refresh, color: Colors.white),
+      ),
     );
-  }
-}
-
-// DataPoint class for the scatter chart
-class TemperatureData {
-  final DateTime timestamp;
-  final double temperature;
-
-  TemperatureData(this.timestamp, this.temperature);
-
-  // Conversion function
-  DataPoint toDataPoint() {
-    return DataPoint(timestamp, temperature);
   }
 }
